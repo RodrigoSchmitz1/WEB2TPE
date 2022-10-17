5e00db3ef410d1e5817c0d7209227faecf905ded
Rodrigo Schmitz <rodri.schmitz@gmail.com>
2022-10-17 20:52:49 -0300

    base actualizada

--- tpe.sql
+++ tpe.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-10-2022 a las 22:14:44
+-- Tiempo de generación: 18-10-2022 a las 01:52:01
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -39,29 +39,59 @@ CREATE TABLE `inversion` (
 --
 
 INSERT INTO `inversion` (`id`, `moneda`, `interes`, `id_plataformas_fk`) VALUES
-(1, 'USDT', 8, 1);
+(8, 'BTC', 4, 1),
+(9, 'ETH', 4, 1),
+(11, 'USDT', 9, 2),
+(12, 'BTC', 3, 4),
+(13, 'ETH', 5, 2),
+(14, 'SOL', 12, 2),
+(15, 'USDT', 10, 3),
+(16, 'BTC', 5, 3),
+(17, 'ETH', 5, 3),
+(18, 'ADA', 12, 3),
+(23, 'XRP', 11, 4);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `plataformas`
+-- Estructura de tabla para la tabla `plataforma`
 --
 
-CREATE TABLE `plataformas` (
+CREATE TABLE `plataforma` (
   `id` int(11) NOT NULL,
   `nombre` varchar(15) NOT NULL,
-  `años_activo` smallint(6) NOT NULL
+  `anios_activo` smallint(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Volcado de datos para la tabla `plataformas`
+-- Volcado de datos para la tabla `plataforma`
 --
 
-INSERT INTO `plataformas` (`id`, `nombre`, `años_activo`) VALUES
+INSERT INTO `plataforma` (`id`, `nombre`, `anios_activo`) VALUES
 (1, 'Binance', 5),
-(2, 'ripio', 9),
+(2, 'Ripio', 7),
 (3, 'Latamex', 5),
 (4, 'Bitso', 8);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `email` text NOT NULL,
+  `password` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `password`) VALUES
+(1, 'r@.com', '12345'),
+(2, 'r@a.com', '$2a$12$wkIBDTfqQK1RB4V1v/geO.SgCBpeFuLj0.wWKVQW01cG2pLAdDLQe');
 
 --
 -- Índices para tablas volcadas
@@ -72,12 +102,18 @@ INSERT INTO `plataformas` (`id`, `nombre`, `años_activo`) VALUES
 --
 ALTER TABLE `inversion`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_plataformas` (`id_plataformas_fk`);
+  ADD KEY `id_plataformas_fk` (`id_plataformas_fk`);
 
 --
--- Indices de la tabla `plataformas`
+-- Indices de la tabla `plataforma`
 --
-ALTER TABLE `plataformas`
+ALTER TABLE `plataforma`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -88,13 +124,19 @@ ALTER TABLE `plataformas`
 -- AUTO_INCREMENT de la tabla `inversion`
 --
 ALTER TABLE `inversion`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
--- AUTO_INCREMENT de la tabla `plataformas`
+-- AUTO_INCREMENT de la tabla `plataforma`
 --
-ALTER TABLE `plataformas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `plataforma`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restricciones para tablas volcadas
@@ -104,7 +146,7 @@ ALTER TABLE `plataformas`
 -- Filtros para la tabla `inversion`
 --
 ALTER TABLE `inversion`
-  ADD CONSTRAINT `fk_plataformas` FOREIGN KEY (`id_plataformas_fk`) REFERENCES `inversion` (`id`);
+  ADD CONSTRAINT `inversion_ibfk_1` FOREIGN KEY (`id_plataformas_fk`) REFERENCES `plataforma` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
